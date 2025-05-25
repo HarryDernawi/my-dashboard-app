4cb9888017a580c2f1b8a32e32622fb9f20e7fff
Academy <dernawiia@gmail.com>
2025-05-25 13:31:07 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-dashboard-app/', // هذا السطر هو الأهم للنشر على GitHub Pages
   plugins: [react()],
 })
